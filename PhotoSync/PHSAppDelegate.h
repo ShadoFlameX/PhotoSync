6a38d5eb6326b5c7eb1ccc44a132e7ab8033d237
Bryan Hansen <bryan@seatme.com>
2012-08-16 23:51:30 -0700

    Initial Project commit with working status item.

---
+++ PhotoSync/PHSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PHSAppDelegate.h
+//  PhotoSync
+//
+//  Created by Bryan Hansen on 8/16/12.
+//  Copyright (c) 2012 skeuo. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface PHSAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
